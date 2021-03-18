e909e69c111caddeb46a5349695f00db4d16a5f1
Dan Mugisho <danmmwx@gmail.com>
2021-03-18 22:39:15 +0300

    Adding a background to the card

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import {Container, CssBaseline, Avatar, Typography, FormControlLabel, Button,
     Checkbox, Grid, Link, makeStyles, createMuiTheme, ThemeProvider, Card, CardContent} from '@material-ui/core';
-import {LockRounded} from '@material-ui/icons';
+import {LockRounded, LockSharp} from '@material-ui/icons';
 import {ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 import fire from '../firebase/config';
 import {ToastContainer, toast} from 'react-toastify';
 import {ScaleLoader} from 'react-spinners';
 import { motion } from 'framer-motion';
+import './Auth.css'
 
 const Login = (props) => {
     const classes = useStyles();
@@ -52,114 +53,116 @@ const Login = (props) => {
 
     }
     return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <Card className={classes.card}>
-                    <CardContent>
-                        <ToastContainer/>
-                        <CssBaseline/>
-                        <div className={classes.paper}>
-                            <Avatar className={classes.avatar}>
-                                <LockRounded />
-                            </Avatar>
-                            <Typography component="h1" variant="h5" className={classes.mainTitle} >
-                                Sign In
-                            </Typography>
-                            <ValidatorForm
-                                onSubmit={handlerLogin}
-                                onError={errors => {
-                                    for (const err of errors) {
-                                        console.log(err.props.errorMessages[0])
-                                    }
-                                }}
-                                className={classes.form}>
-                                <TextValidator
-                                    variant="standard"
-                                    margin="normal"
+        <div className="bg-img">
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xs" className={classes.container}>
+                    <Card className={classes.card}>
+                        <CardContent>
+                            <ToastContainer/>
+                            <CssBaseline/>
+                            <div className={classes.paper}>
+                                <Avatar className={classes.avatar}>
+                                    <LockRounded />
+                                </Avatar>
+                                <Typography component="h1" variant="h5" className={classes.mainTitle} >
+                                    Sign In
+                                </Typography>
+                                <ValidatorForm
+                                    onSubmit={handlerLogin}
+                                    onError={errors => {
+                                        for (const err of errors) {
+                                            console.log(err.props.errorMessages[0])
+                                        }
+                                    }}
+                                    className={classes.form}>
+                                    <TextValidator
+                                        variant="standard"
+                                        margin="normal"
 
-                                    fullWidth
-                                    label="Email"
-                                    onChange={handleEmail}
-                                    name="email"
-                                    value={email}
-                                    validators={['required', 'isEmail']}
-                                    errorMessages={['this field is required', 'email is not valid']}
-                                    autoComplete='off'
-                                    InputProps={{
-                                        className: classes.label,
-                                    }}
-                                />
-                                <TextValidator
-                                    variant="standard"
-                                    fullWidth
-                                    label="Password"
-                                    onChange={handlePassword}
-                                    name="password"
-                                    type="password"
-                                    value={password}
-                                    validators={['required']}
-                                    errorMessages={['this field is required']}
-                                    autoComplete="off"
-                                    InputProps={{
-                                        className: classes.label,
-                                    }}
-                                />
-                                <br/>
-                                <FormControlLabel
-                                    control={
-                                        <motion.div
-                                            whileHover={{ scale: 1.2, rotate: 90 }}
-                                            whileTap={{
-                                                scale: 0.8,
-                                                rotate: -90,
-                                                borderRadius: "100%"
-                                            }}
-                                        >
-                                            <Checkbox
+                                        fullWidth
+                                        label="Email"
+                                        onChange={handleEmail}
+                                        name="email"
+                                        value={email}
+                                        validators={['required', 'isEmail']}
+                                        errorMessages={['this field is required', 'email is not valid']}
+                                        autoComplete='off'
+                                        InputProps={{
+                                            className: classes.label,
+                                        }}
+                                    />
+                                    <TextValidator
+                                        variant="standard"
+                                        fullWidth
+                                        label="Password"
+                                        onChange={handlePassword}
+                                        name="password"
+                                        type="password"
+                                        value={password}
+                                        validators={['required']}
+                                        errorMessages={['this field is required']}
+                                        autoComplete="off"
+                                        InputProps={{
+                                            className: classes.label,
+                                        }}
+                                    />
+                                    <br/>
+                                    <FormControlLabel
+                                        control={
+                                            <motion.div
                                                 whileHover={{ scale: 1.2, rotate: 90 }}
                                                 whileTap={{
                                                     scale: 0.8,
                                                     rotate: -90,
                                                     borderRadius: "100%"
                                                 }}
-                                                value={rememberMe}
-                                                onChange={(e) => handleCheck(e)}
-                                                color="primary" />
-                                        </motion.div>
-                                    }
-                                    label="Remember me"
-                                />
-                                {loading ? (
-                                    <ScaleLoader
-                                        css={override}
-                                        size={150}
-                                        color={"#eb4034"}
-                                        loading={loading}/>
-                                ) : (
-                                    <Button
-                                        type="submit"
-                                        fullWidth
-                                        variant="contained"
-                                        className={classes.submit}
-                                    >
-                                        Sign In
-                                    </Button>
+                                            >
+                                                <Checkbox
+                                                    whileHover={{ scale: 1.2, rotate: 90 }}
+                                                    whileTap={{
+                                                        scale: 0.8,
+                                                        rotate: -90,
+                                                        borderRadius: "100%"
+                                                    }}
+                                                    value={rememberMe}
+                                                    onChange={(e) => handleCheck(e)}
+                                                    color="primary" />
+                                            </motion.div>
+                                        }
+                                        label="Remember me"
+                                    />
+                                    {loading ? (
+                                        <ScaleLoader
+                                            css={override}
+                                            size={150}
+                                            color={"#eb4034"}
+                                            loading={loading}/>
+                                    ) : (
+                                        <Button
+                                            type="submit"
+                                            fullWidth
+                                            variant="contained"
+                                            className={classes.submit}
+                                        >
+                                            Sign In
+                                        </Button>
 
-                                )}
-                                <Grid container>
-                                    <Grid item>
-                                        <br/>
-                                        <Link  onClick={props.toggle} className={classes.pointer} variant="body2">
-                                            {"Don't have an account? Sign Up"}
-                                        </Link>
+                                    )}
+                                    <Grid container>
+                                        <Grid item>
+                                            <br/>
+                                            <Link  onClick={props.toggle} className={classes.pointer} variant="body2">
+                                                {"Don't have an account? Sign Up"}
+                                            </Link>
+                                        </Grid>
                                     </Grid>
-                                </Grid>
-                            </ValidatorForm>
-                        </div>
-                    </CardContent>
-                </Card>
-            </Container>
-        </ThemeProvider>
+                                </ValidatorForm>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </Container>
+            </ThemeProvider>
+        </div>
 
     );
 }
@@ -177,11 +180,17 @@ const theme = createMuiTheme({
 
 
 const useStyles = makeStyles((theme) => ({
+    container: {
+        // display: 'flex',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+    },
     paper: {
-        marginTop: theme.spacing(8),
+        marginTop: theme.spacing(10),
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
+
     },
     avatar: {
         margin: theme.spacing(1),
@@ -191,7 +200,9 @@ const useStyles = makeStyles((theme) => ({
     mainTitle : {
         fontFamily: 'Poiret One',
         fontSize: '30px',
-        fontWeight: '600'
+        fontWeight: '600',
+        color: '#0e0e0e',
+        // color: '#fff',
     },
     form: {
         width: '100%',
@@ -202,7 +213,7 @@ const useStyles = makeStyles((theme) => ({
         fontFamily: 'Montserrat',
         fontSize: '15px',
         fontWeight: 'bold',
-        color: '#645c5c',
+        color: '#0e0e0e',
     },
     submit: {
         background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
@@ -217,15 +228,26 @@ const useStyles = makeStyles((theme) => ({
         paddingLeft: '20px',
         paddingRight: '20px',
         paddingBottom: '20px',
+        background: 'linear-gradient(196deg, rgba(244,67,54,1) 0%, rgba(254,107,139,1) 18%, rgba(255,255,255,0) 70%)',
+        boxShadow: '-1px 5px 18px 0px rgba(0,0,0,0.75)',
+        // boxShadow: '0px 2px 7px rgba(0,0,0,0.3)',
+
+        ['@media (max-width:480px)']: {
+            paddingLeft: '10px',
+            paddingRight: '10px',
+        }
     },
     pointer: {
         cursor: 'pointer',
-        color: '#fe7678',
+        // color: '#fe7678',
+        color: '#f44336',
         fontFamily: 'Montserrat',
         fontSize: '15px',
         fontWeight: 'bold'
 
-    }
+    },
+
+
 }));
 
 export default Login;
--- src/auth/SignUp.js
+++ src/auth/SignUp.js
@@ -8,6 +8,7 @@ import {ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 import fire from '../firebase/config';
 import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
+import './Auth.css'
 
 
 const SignUp = (props) => {
@@ -61,89 +62,91 @@ const SignUp = (props) => {
     }, [password])
 
     return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <Card className={classes.card}>
-                    <CardContent>
-                        <ToastContainer/>
-                        <CssBaseline/>
-                        <div className={classes.paper}>
-                            <Avatar className={classes.avatar}>
-                                <LockRounded/>
-                            </Avatar>
-                            <Typography component="h1" variant="h5" className={classes.mainTitle}>
-                                Sign Up
-                            </Typography>
-                            <ValidatorForm
-                                onSubmit={handleSignUp}
-                                className={classes.form}>
-                                <TextValidator
-                                    variant="standard"
-                                    margin="normal"
-                                    fullWidth
-                                    label="Email"
-                                    onChange={handleEmail}
-                                    name="email"
-                                    value={email}
-                                    validators={['required', 'isEmail']}
-                                    errorMessages={['this field is required', 'email is not valid']}
-                                    autoComplete='off'
-                                    InputProps={{
-                                        className: classes.label,
-                                    }}
-                                />
-                                <br/>
-                                <TextValidator
-                                    variant="standard"
-                                    fullWidth
-                                    label="Password"
-                                    onChange={handlePassword}
-                                    name="password"
-                                    type="password"
-                                    value={password}
-                                    validators={['required']}
-                                    errorMessages={['this field is required']}
-                                    autoComplete="off"
-                                    InputProps={{
-                                        className: classes.label,
-                                    }}
-                                />
-                                <br/>
-                                <TextValidator
-                                    variant="standard"
-                                    label="Confirm password"
-                                    fullWidth
-                                    onChange={handleConfirmPassword}
-                                    name="confirmPassword"
-                                    type="password"
-                                    validators={['isPasswordMatch', 'required']}
-                                    errorMessages={['password mismatch', 'this field is required']}
-                                    value={confirmPassword}
-                                    autoComplete="off"
-                                />
-                                <br/>
-                                <Button
-                                    type="submit"
-                                    fullWidth
-                                    variant="contained"
-                                    className={classes.submit}
-                                >
+        <div className="bg-img">
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xs">
+                    <Card className={classes.card}>
+                        <CardContent>
+                            <ToastContainer/>
+                            <CssBaseline/>
+                            <div className={classes.paper}>
+                                <Avatar className={classes.avatar}>
+                                    <LockRounded/>
+                                </Avatar>
+                                <Typography component="h1" variant="h5" className={classes.mainTitle}>
                                     Sign Up
-                                </Button>
-                                <Grid container>
-                                    <Grid item>
-                                        <br/>
-                                        <Link onClick={props.toggle} className={classes.pointer} variant="body2">
-                                            {"Already have an account? Sign In"}
-                                        </Link>
+                                </Typography>
+                                <ValidatorForm
+                                    onSubmit={handleSignUp}
+                                    className={classes.form}>
+                                    <TextValidator
+                                        variant="standard"
+                                        margin="normal"
+                                        fullWidth
+                                        label="Email"
+                                        onChange={handleEmail}
+                                        name="email"
+                                        value={email}
+                                        validators={['required', 'isEmail']}
+                                        errorMessages={['this field is required', 'email is not valid']}
+                                        autoComplete='off'
+                                        InputProps={{
+                                            className: classes.label,
+                                        }}
+                                    />
+                                    <br/>
+                                    <TextValidator
+                                        variant="standard"
+                                        fullWidth
+                                        label="Password"
+                                        onChange={handlePassword}
+                                        name="password"
+                                        type="password"
+                                        value={password}
+                                        validators={['required']}
+                                        errorMessages={['this field is required']}
+                                        autoComplete="off"
+                                        InputProps={{
+                                            className: classes.label,
+                                        }}
+                                    />
+                                    <br/>
+                                    <TextValidator
+                                        variant="standard"
+                                        label="Confirm password"
+                                        fullWidth
+                                        onChange={handleConfirmPassword}
+                                        name="confirmPassword"
+                                        type="password"
+                                        validators={['isPasswordMatch', 'required']}
+                                        errorMessages={['password mismatch', 'this field is required']}
+                                        value={confirmPassword}
+                                        autoComplete="off"
+                                    />
+                                    <br/>
+                                    <Button
+                                        type="submit"
+                                        fullWidth
+                                        variant="contained"
+                                        className={classes.submit}
+                                    >
+                                        Sign Up
+                                    </Button>
+                                    <Grid container>
+                                        <Grid item>
+                                            <br/>
+                                            <Link underline='none' onClick={props.toggle} className={classes.pointer} variant="body2">
+                                                {"Already have an account? Sign In"}
+                                            </Link>
+                                        </Grid>
                                     </Grid>
-                                </Grid>
-                            </ValidatorForm>
-                        </div>
-                    </CardContent>
-                </Card>
-            </Container>
-        </ThemeProvider>
+                                </ValidatorForm>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </Container>
+            </ThemeProvider>
+        </div>
 
     );
 }
@@ -161,7 +164,7 @@ const theme = createMuiTheme({
 
 const useStyles = makeStyles((theme) => ({
     paper: {
-        marginTop: theme.spacing(8),
+        marginTop: theme.spacing(10),
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
@@ -183,7 +186,7 @@ const useStyles = makeStyles((theme) => ({
         fontFamily: 'Montserrat',
         fontSize: '15px',
         fontWeight: 'bold',
-        color: '#645c5c',
+        color: '#0e0e0e',
     },
     submit: {
         background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
@@ -195,12 +198,22 @@ const useStyles = makeStyles((theme) => ({
     },
     card: {
         marginTop: '60px',
-        paddingLeft: '30px',
-        paddingRight: '30px',
+        paddingLeft: '20px',
+        paddingRight: '20px',
         paddingBottom: '20px',
+        background: 'linear-gradient(201deg, rgba(244,67,54,1) 0%, rgba(254,107,139,1) 18%, rgba(255,255,255,0) 67%)',
+        boxShadow: '-1px 5px 18px 0px rgba(0,0,0,0.75)',
+        // boxShadow: '0px 2px 7px rgba(0,0,0,0.3)',
+
+        ['@media (max-width:480px)']: {
+            paddingLeft: '10px',
+            paddingRight: '10px',
+        }
     },
     pointer: {
-        color: '#fe7678',
+        cursor: 'pointer',
+        // color: '#fe7678',
+        color: '#f44336',
         fontFamily: 'Montserrat',
         fontSize: '15px',
         fontWeight: 'bold'
